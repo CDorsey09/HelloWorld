b4a9e0e25831bd97eab7b867f24e1115b9187bf8
CDorsey09 <chrisdorsey09@gmail.com>
2013-10-08 23:51:05 -0400

    Initial Commit of Hello World Files

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,11 @@
+class HelloController < ApplicationController
+  
+
+def World
+  
+end
+
+<h1> Hello World! </h1>
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '08c6b78f12e470aee0e9083af06bb8ae383179d9f8d197c1ebeb519f3c3c4e6afb88394dcce62ea06cac285f50487c3c0f2d8025d7f74e395ec476dcb7860732'
